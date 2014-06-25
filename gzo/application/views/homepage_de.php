148628047b156b10ab8b7bfeafcd4c801dc18ffe
Lukas Oppermann <lukas@vea.re>
2014-06-25 20:50:23 +0200

    update slider

--- gzo/application/views/homepage_de.php
+++ gzo/application/views/homepage_de.php
@@ -28,15 +28,14 @@
 
 <div class="branchen">
 	<h2><a name="branchen" id="branchen" class="anchor">Branchen</a></h2>
-  <p class="intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p class="intro last">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p class="intro">Perfekte Oberflächen finden in nahezu allen Bereichen der Technik und des Alltags große Beachtung. Hier finden Sie einen Überblick, in welchen Branchen die Schwerpunkte unserer Aktivitäten liegen.</p>
 	<div class="branche">
     <div class="img">
 		  <img src="<?=base_url().'media/images/branchen/antriebstechnik.png'?>" alt="Antriebstechnik" />
     </div>
 		<div class="copy">
       <h3>Antriebstechnik</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Mechanische Komponenten in der Antriebstechnik erfordern eine hohe Oberflächengüte und definierte Kantenkonturen. Dafür bieten wir die passenden Prozesse, präzise, zuverlässig, stabil und reproduzierbar.</p>
     </div>
 	</div>
 	<div class="branche">
@@ -45,25 +44,16 @@
     </div>
     <div class="copy">
       <h3>Automobil</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Oberflächen rund um die Automobil- produktion. Funktional und dekorativ. Interieur und Exterieur. Für viele Anforderungen haben wir eine passende Lösung. 20 Jahre Automobil- erfahrung geben Ihnen die Sicherheit, die Sie von einem Dienstleister erwarten.</p>
     </div>
 	</div>
 	<div class="branche last">
-    <div class="img">
-		  <img src="<?=base_url().'media/images/branchen/deko.png'?>" alt="Dekorative Oberflächen" />
-    </div>
-    <div class="copy">
-		  <h3>Dekorative Oberflächen</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-	</div>
-	<div class="branche last-row">
     <div class="img">
 		  <img src="<?=base_url().'media/images/branchen/lebensmitteltechnik.png'?>" alt="Lebensmitteltechnik" />
     </div>
     <div class="copy">
       <h3>Lebensmitteltechnik</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Einheitliche und perfekte Oberflächen, optimale Reinigungseigenschaften und eine ansehnliche Optik spielen in der Lebensmitteltechnik eine wichtige Rolle. Wir bieten Ihnen die passenden Prozesse und Produktionskapazitäten.</p>
     </div>
 	</div>
 	<div class="branche last-row">
@@ -72,16 +62,25 @@
     </div>
     <div class="copy">
       <h3>Medizintechnik</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>In der Medizintechnik, z.B. bei der Herstellung von Gelenkimplantaten oder chirurgischen Instrumenten, sind perfekte Oberflächen ein wesentliches Qualitätsmerkmal. Wir erfüllen mit unseren Verfahren die hohen Anforderungen für medizintechnische Produkte.</p>
+    </div>
+	</div>
+	<div class="branche last-row">
+    <div class="img">
+		  <img src="<?=base_url().'media/images/branchen/deko.png'?>" alt="Dekorative Oberflächen" />
+    </div>
+    <div class="copy">
+		  <h3>Dekorative Oberflächen</h3>
+      <p>Polierte Oberflächen sind in der Regel stilvoller und haltbarer als galvanisch beschichte Oberflächen. Wir verfügen über zahlreiche Möglichkeiten moderner Schleif- und Polier- und Strahltechnologien für die dekorative Oberflächengestaltung für Metalle und Kunststoffe.</p>
     </div>
 	</div>
 	<div class="branche last last-row">
     <div class="img">
-      <img src="<?=base_url().'media/images/branchen/teilefertigung.png'?>" alt="Teilefertigung" />
+      <img src="<?=base_url().'media/images/branchen/teilefertigung.png'?>" alt="Serienteilfertigung" />
     </div>
     <div class="copy">
-      <h3>Teilefertigung</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h3>Serienteilfertigung</h3>
+      <p>Unsere Produktionskapazitäten ermöglichen durch einen umfangreichen Maschinenpark und ein etabliertes 3-Schichtsystem neben der Bearbeitung von kleinen und mittleren Serien auch die Bearbeitung von Teilen aus der Großserienfertigung.</p>
     </div>
 	</div>
 </div>
